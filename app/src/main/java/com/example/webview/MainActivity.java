2850b8827e019fa4e91321154563d6471868b9c4
Orex-x <vipboy78038@gmail.com>
2021-12-21 14:43:14 +0300

    init commit

---
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        webView = findViewById(R.id.webView);
+        // включаем поддержку JavaScript
+        webView.getSettings().setJavaScriptEnabled(true);
+        // указываем страницу загрузки
+        webView.loadUrl("https://start.duckduckgo.com/");
+
+        webView.setWebViewClient(new MyWebViewClient());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
+
+ class MyWebViewClient extends WebViewClient {
+    @TargetApi(Build.VERSION_CODES.N)
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+        view.loadUrl(request.getUrl().toString());
+        return true;
+    }
+
+    // Для старых устройств
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        view.loadUrl(url);
+        return true;
+    }
+
+
+}
